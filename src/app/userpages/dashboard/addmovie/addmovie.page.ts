a595a51da0ccf46abeb950bd84c3ca342b74de4b
Andras <nemethand94@gmail.com>
2022-02-24 20:32:50 +0100

    use IMovie in addmovie.page

--- src/app/userpages/dashboard/addmovie/addmovie.page.ts
+++ src/app/userpages/dashboard/addmovie/addmovie.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Movie } from 'src/app/classes/movie';
+
+//interfaces
+import { IMovie } from 'src/app/interfaces/export-interfaces';
+
+//services
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -9,37 +13,33 @@ import { MoviesService } from 'src/app/services/movies.service';
   styleUrls: ['./addmovie.page.scss'],
 })
 export class AddmoviePage implements OnInit {
-  defaultThumbnailURL =
-    'https://www.freeiconspng.com/uploads/no-image-icon-13.png';
+
+  newMovie: IMovie;
+  movies: IMovie[];
+
+  defaultThumbnailURL = 'https://www.freeiconspng.com/uploads/no-image-icon-13.png';
   firstName = localStorage.getItem('auth-token').slice(13);
   defaultImageSRC = this.defaultThumbnailURL;
-  //for ID generation
+
   storageLength = localStorage.getItem('movies').length;
-  newMovie: Movie = new Movie(
-    this.storageLength + 1,
-    '',
-    null,
-    null,
-    this.firstName,
-    '',
-    ''
-  );
-  movies: Movie[];
 
   constructor(private router: Router, private movieService: MoviesService) {}
   backToDashBoard() {
     this.router.navigate(['userpages/dashboard']);
   }
 
+  ngOnInit() {
+
+  }
+
   onSubmit() {
     this.movieService.addMovie(this.newMovie);
-
     return this.movies;
   }
+
   watchThumbnailInput($event) {
     console.log('new thumb: ' + $event.target.value);
 
     this.defaultImageSRC = $event.target.value;
   }
-  ngOnInit() {}
 }
